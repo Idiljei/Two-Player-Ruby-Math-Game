422c4be81739146fefa4e68f5ce1de0fd60b124d
Idiljei <i.jeilani@gmail.com>
2021-03-01 20:13:04 +0000

    ask method

--- questions.rb
+++ questions.rb
@@ -1,20 +1,28 @@
+# Methods to generate generate questions and see if it's correct
 class Questions 
-  attr_writer :score
+  attr_writer :num1, :num2, :sum
 
-  def initialize(score)
-    @score = score
+  # randomize two numbers from 1-20 and sum 
+  def initialize()
+    @num1 = rand(1..20) 
+    @num2 = rand(1..20)
+    @sum = @num1 + @num2
   end
+
 
   def ask(question, answer)
     print question
-    user_answer = gets.chomp
-    if user_answer == answer
-        puts "Correct!"
-        @score += 1
-    else
-        puts "Wrong! The answer was #{answer}"
+      user_answer = gets.chomp.to_i
+        if user_answer == self.sum
+            puts "Correct!"
+            @score += 1
+        else
+            puts "Wrong! The answer was #{answer}"
+        end
     end
-end
+      
+    end 
+  
 end
 
 
